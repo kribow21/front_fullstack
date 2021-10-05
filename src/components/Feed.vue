4e7408d815dd5441a531d2653dedcc8f6373fe90
kribow21 <kristininnotech@gmail.com>
2021-10-05 14:10:04 -0600

    setting up new components

---
+++ src/components/Feed.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <FeedPost></FeedPost>
+    </div>
+</template>
+
+<script>
+import FeedPost from "./FeedPost.vue"
+    export default {
+        name : "Feed",
+        components: {
+            FeedPost
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
