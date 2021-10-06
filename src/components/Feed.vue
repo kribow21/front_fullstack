ebbe74eecebd6ad6f2fa7ccd36373b8ec90311ab
kribow21 <kristininnotech@gmail.com>
2021-10-06 10:35:32 -0600

    forgot to install vuetify

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
+        <h1>Home Feed</h1>
+        <v-textarea
+            outlined
+            clearable
+            name="input-7-4"
+            label="Create A Post"
+            v-model="tweetInput"
+        ></v-textarea>
+        <v-btn
+            color="primary"
+            elevation="2"
+            raised
+        >Tweet</v-btn>
         <FeedPost></FeedPost>
     </div>
 </template>
